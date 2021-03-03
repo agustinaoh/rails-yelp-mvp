72bf8273ecd2960cced30a93b32c25eba966e71d
agustina ohan <agustina.oh@gmail.com>
2021-03-03 16:32:59 +0100

    ready for front end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,2 @@
 class ApplicationController < ActionController::Base
- 
-  def index
-    @restaurants = Restaurant.all
-  end
-
-  def new
-    @restaurant = Restaurant.new
-  end
-
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
-
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
-  end
-
-  private
-
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
-  end
 end
